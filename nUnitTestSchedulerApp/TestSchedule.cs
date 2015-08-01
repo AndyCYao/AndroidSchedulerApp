8a9ce05f42d2d89977ea23419d2f55943566fc0a
Andy <andyyao3@gmail.com>
2015-08-01 10:36:08 -0700

    learned about nUnit test in xamarin, created a test for checking task name and task notes

---
+++ nUnitTestSchedulerApp/TestSchedule.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using SchedulerTask;
+
+namespace nUnitTestSchedulerApp
+{
+	[TestFixture]
+	public class TestClass
+	{
+		[Test]
+		public void TestCreatingTaskObject ()
+		{
+				string TestTaskName = "PickUpBooks";
+				string TestTaskNote = "BooksAtLibrary";
+				Task GetBooks = new Task (TestTaskName,TestTaskNote);
+				Console.WriteLine (GetBooks.TaskNotes);
+				Assert.AreEqual (TestTaskNote, GetBooks.TaskNotes);
+				Assert.AreEqual (TestTaskName, GetBooks.TaskName);
+		}
+	}
+}
+
