068087887342f9c808e1f5fc8d523ee3fc4948e2
Andy <andyyao3@gmail.com>
2015-08-25 19:18:26 -0700

    Testing adding new forms to the scheduler app, including the Main page, and Task Viewer page

---
+++ ScheduleApp/ScheduleApp/ScheduleApp.Droid/Resources/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ScheduleApp.Droid
+{
+	public class Task : ContentPage
+	{
+		public Task ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
