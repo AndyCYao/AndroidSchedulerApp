cf87c62ed071c3cc84197a9dea5adfc9484bb7b9
Andy <andyyao3@gmail.com>
2015-09-04 18:21:17 -0700

    Added new controls into the AddTask page

---
+++ ScheduleApp/ScheduleApp/ScheduleApp/Views/pageAddTask.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+//Add Task viewer
+//Sept 4th 2015
+//Controls are listed in this document below
+//https://developer.xamarin.com/guides/cross-platform/xamarin-forms/controls/views/
+namespace ScheduleApp
+{
+	public class pageAddTask:ContentPage
+	{
+		public pageAddTask ()
+		{
+			Title = "Add Task";
+			var nameLabel = new Label { Text = "Task Name" };
+			var nameEntry = new Entry ();
+			nameEntry.SetBinding (Entry.TextProperty, "Task Name");
+
+			var noteLabel = new Label { Text = "Task Note" };
+			var noteEntry = new Entry ();
+			nameEntry.SetBinding (Entry.TextProperty, "Task Note");
+
+			var doneLabel = new Label { Text = "Done" };
+			var donePicker = new Picker{};
+			donePicker.Items.Add ("Yes");
+			donePicker.Items.Add ("No");
+
+			var reminderEndDateLabel = new Label{ Text = "Set End Date" };
+			var reminderEndDatePicker = new DatePicker{ 
+				Format= "D",
+			};
+
+			Content = new StackLayout {
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				Padding = new Thickness (20),
+				Children = {
+					nameLabel,
+					nameEntry,
+					noteLabel,
+					noteEntry,
+					doneLabel,
+					donePicker,
+					reminderEndDateLabel,
+					reminderEndDatePicker
+				}
+			};
+		}
+	}
+}
+
---
+++ ScheduleApp/ScheduleApp/ScheduleApp/Views/pageMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+//Created by Andy- Sept 4th 2015
+//main page that will greet the user.
+namespace ScheduleApp
+{
+	// The root page of your application
+	public class Main: ContentPage
+	{
+		public Main(){
+			Title = "Scheduler App 2015";
+
+			var AddTask = new Button {
+				Text = "Add Task"
+				//Font = Font.SystemFontOfSize (NamedSize.Large),
+				//BorderWidth = 1,
+				//HorizontalOptions = LayoutOptions.Center,
+				//VerticalOptions = LayoutOptions.CenterAndExpand
+			};
+			AddTask.Clicked += (sender, e) => {
+				var pAddTask = new pageAddTask();
+				this.Navigation.PushAsync(pAddTask);
+			};
+			var SettingButton = new Button { Text = "Setting" };
+			SettingButton.Clicked += (sender, e) => {
+//				go to Settings Page
+//				var todoItem = (TodoItem)BindingContext;
+//				this.Navigation.PopAsync();
+			};
+
+				
+			Content = new StackLayout {
+				VerticalOptions = LayoutOptions.Center,
+				Children = {
+					AddTask,
+					SettingButton
+				}
+			};
+		}
+	};
+}
+
