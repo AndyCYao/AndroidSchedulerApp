4e4a3986796457f033e14e4c189e518027c03e1c
Andy <andyyao3@gmail.com>
2015-09-23 21:48:50 -0700

    Added Core reference in MainPage, reference GetActiveTask and assign it to ListView. However was unable to test it since I couldnt get the Android Player to open SchedulerApp

--- ScheduleApp/ScheduleApp/ScheduleApp/Views/pageMain.cs
+++ ScheduleApp/ScheduleApp/ScheduleApp/Views/pageMain.cs
@@ -19,15 +19,21 @@ namespace ScheduleApp
 		ListView listView; //This is to list all the current tasks in our XML memory.
 		public Main(){
 			Title = "Scheduler App 2015";
-			listView = new ListView ();
-			//below needs a DataTemplateType (typeOfScheduler probably)
-			//listView.ItemTemplate = new DataTemplate(Scheduler);
 
 			//Sept 21. 15 the List view needs to be populated with existings tasks. 
 			//users will have the option to view the tasks, and do actions to them when they click the 
 			//specific task. 
-			Scheduler mainScheduler = Core.GetScheduler();
+			Core MainCore = Core.GetCore();
+			Scheduler MainScheduler = MainCore.GetScheduler ();
+			List<Task> TasksList = MainScheduler.GetActiveTasks ();
 
+			//Info on ListView are in this reference
+			//https://developer.xamarin.com/guides/cross-platform/xamarin-forms/user-interface/listview/data-and-databinding/
+			listView = new ListView ();
+			//below needs a DataTemplateType (typeOfScheduler probably)
+			//listView.ItemTemplate = new DataTemplate(TasksList);
+			//I'm replacing above with .ItemSource to Tasks
+			listView.ItemsSource = TasksList;
 
 
 			//On click of the item it pushs to a task page.
