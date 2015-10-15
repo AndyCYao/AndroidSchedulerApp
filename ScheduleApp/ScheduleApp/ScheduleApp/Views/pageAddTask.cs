c0216c73749ccc80ae8defd8af9a0cef9a49ab58
Andy <andyyao3@gmail.com>
2015-10-15 00:18:48 -0700

    Include default values for the pickers, DatePicker and Text now works

--- ScheduleApp/ScheduleApp/ScheduleApp/Views/pageAddTask.cs
+++ ScheduleApp/ScheduleApp/ScheduleApp/Views/pageAddTask.cs
@@ -18,125 +18,139 @@ using System.Diagnostics;
 //http://developer.xamarin.com/guides/cross-platform/xamarin-forms/user-interface/tableview/
 namespace ScheduleApp
 {
-    public class pageAddTask:ContentPage
-    {
+	public class pageAddTask:ContentPage
+	{
 
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
 
-            Style = Core.GetCore().GetConfig().GeneratePageStyle();
-        }
+			Style = Core.GetCore().GetConfig().GeneratePageStyle();
+		}
 
-        public pageAddTask()
-        {
-            AppConfig config = Core.GetCore().GetConfig();
-            
-            Title = "Add Task With ScrollView";
-            var nameLabel = new Label { Text = "Task Name", Style = config.GenerateLabelStyle() };
-            
-            var nameEntry = new Entry {Placeholder = "New Task Name"};
-            nameEntry.Style = config.GenerateEntryStyle();
-            nameEntry.SetBinding (Entry.TextProperty, "Task Name");
+		public pageAddTask()
+		{
+			AppConfig config = Core.GetCore ().GetConfig ();
 
-            var noteLabel = new Label { Text = "Task Note", Style = config.GenerateLabelStyle() };
-            var noteEntry = new Entry {Placeholder = "New Task Notes"};
-            noteEntry.Style = config.GenerateEntryStyle();
-            noteEntry.SetBinding (Entry.TextProperty, "Task Note");
+			Title = "Add Task With ScrollView";
+			var nameLabel = new Label { Text = "Task Name", Style = config.GenerateLabelStyle () };
 
-            var doneLabel = new Label { Text = "Done", Style = config.GenerateLabelStyle() };
-            var donePicker = new Picker{ Style = config.GeneratePickerStyle() };
-            donePicker.Items.Add ("Yes");
-            donePicker.Items.Add ("No");
+			var nameEntry = new Entry { Placeholder = "New Task Name" };
+			nameEntry.Style = config.GenerateEntryStyle ();
+			nameEntry.SetBinding (Entry.TextProperty, "Task Name");
 
-            var reminderEndDateLabel = new Label{ Text = "Set End Date", Style = config.GenerateLabelStyle() };
-            var reminderEndDatePicker = new DatePicker{ 
-                Format= "D",
-                Style = config.GeneratePickerStyle()
-            };
+			var noteLabel = new Label { Text = "Task Note", Style = config.GenerateLabelStyle () };
+			var noteEntry = new Entry { Placeholder = "New Task Notes" };
+			noteEntry.Style = config.GenerateEntryStyle ();
+			noteEntry.SetBinding (Entry.TextProperty, "Task Note");
 
-            var ringToneLabel = new Label { Text = "Select Ringtone", Style = config.GenerateLabelStyle() };
-            var ringTonePicker = new Picker{ Style = config.GeneratePickerStyle() };
+			var doneLabel = new Label { Text = "Done", Style = config.GenerateLabelStyle () };
+			var donePicker = new Picker{ Style = config.GeneratePickerStyle () };
+			donePicker.Items.Add ("true");
+			donePicker.Items.Add ("false");
+			donePicker.SelectedIndex = 0;
+			var reminderEndDateLabel = new Label{ Text = "Set End Date", Style = config.GenerateLabelStyle () };
+			var reminderEndDatePicker = new DatePicker { 
+				Format = "D",
+				Style = config.GeneratePickerStyle ()
+			};
 
-            //This part we can create a dictionary object that contains all the
-            //ringtones, then load it into this picker, until then we just include
-            //these three. 
-            ringTonePicker.Items.Add ("Crazy Frog");
-            ringTonePicker.Items.Add ("Minions");
-            ringTonePicker.Items.Add ("Flight of the Valkryie");
+			var ringToneLabel = new Label { Text = "Select Ringtone", Style = config.GenerateLabelStyle () };
+			var ringTonePicker = new Picker{ Style = config.GeneratePickerStyle () };
 
-            var frequencyLabel = new Label { Text = "Select Frequency", Style = config.GenerateLabelStyle() };
-            var frequencyPicker = new Picker { Style = config.GeneratePickerStyle() };
+			//This part we can create a dictionary object that contains all the
+			//ringtones, then load it into this picker, until then we just include
+			//these three. 
+			ringTonePicker.Items.Add ("Crazy Frog");
+			ringTonePicker.Items.Add ("Minions");
+			ringTonePicker.Items.Add ("Flight of the Valkryie");
 
-            for (int i = 1; i <= 10; i++) {
-            
-                frequencyPicker.Items.Add(i.ToString());
-            };
+			//This is subject to change, need to speak to Peter
+			//on how the default ringtone in the config page gets updated here
+			ringTonePicker.SelectedIndex = 0;
 
-            var frequencyUnitLabel = new Label { Text = "Select Unit", Style = config.GenerateLabelStyle() };
-            var frequencyUnitPicker = new Picker{ Style = config.GeneratePickerStyle() };
+			var frequencyLabel = new Label { Text = "Select Frequency", Style = config.GenerateLabelStyle () };
+			var frequencyPicker = new Picker { Style = config.GeneratePickerStyle () };
 
-            frequencyUnitPicker.Items.Add ("Minutes");
-            frequencyUnitPicker.Items.Add ("Hours");
-            frequencyUnitPicker.Items.Add ("Days");
-            frequencyUnitPicker.Items.Add ("Weeks");
+			for (int i = 1; i <= 10; i++) {
 
-            var SaveButton = new Button {
-                Text = "Save!",
-                Style = config.GenerateButtonStyle()
-            };
+				frequencyPicker.Items.Add (i.ToString ());
+			}
+			;
+			frequencyPicker.SelectedIndex = 5;
 
-            SaveButton.Clicked += (sender, e) => {
+			var frequencyUnitLabel = new Label { Text = "Select Unit", Style = config.GenerateLabelStyle () };
+			var frequencyUnitPicker = new Picker{ Style = config.GeneratePickerStyle () };
 
-                string tTaskName;
-                string tTaskNotes;
-                DateTime tReminderEndDate;
-                bool tDone;
-                string tRingToneName;
-                string tFrequency;
-                string tFrequencyUnit;
+			frequencyUnitPicker.Items.Add ("Minutes");
+			frequencyUnitPicker.Items.Add ("Hours");
+			frequencyUnitPicker.Items.Add ("Days");
+			frequencyUnitPicker.Items.Add ("Weeks");
 
-                //tTaskName= nameEntry.Text.ToString();
-                //tTaskNotes = noteEntry.Text.ToString();
-                //tReminderEndDate = reminderEndDatePicker.DateSelected;
-                //tDone = donePicker.Items[donePicker.SelectedIndex];
-                tRingToneName = ringTonePicker.Items[ringTonePicker.SelectedIndex];
-                //tFrequency = frequencyPicker.Items[frequencyPicker.SelectedIndex];
-                //tFrequencyUnit = frequencyUnitPicker.Items[donePicker.SelectedIndex];
+			frequencyUnitPicker.SelectedIndex = 2;
 
-                string Results;
-                Results =  tRingToneName + " ";
-                DisplayAlert("Hello, Testing",Results,"Ok");
-                //Push the information presented into Scheduler Class. 
-                //Scheduler SchAdd = new Scheduler();
-                //SchAdd.AddTaskWithInfo();    
-            };
+			var SaveButton = new Button {
+				Text = "Save!",
+				Style = config.GenerateButtonStyle ()
+			};
 
-            ScrollView scrollView = new ScrollView {
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                Content = new StackLayout {
-                    VerticalOptions = LayoutOptions.CenterAndExpand,
-                    Padding = new Thickness (20),
-                    Children = {
-                        nameLabel,
-                        nameEntry,
-                        noteLabel,
-                        noteEntry,
-                        doneLabel,
-                        donePicker,
-                        ringToneLabel,
-                        ringTonePicker,
-                        frequencyLabel,
-                        frequencyPicker,
-                        frequencyUnitLabel,
-                        frequencyUnitPicker,
-                        reminderEndDateLabel,
-                        reminderEndDatePicker,
-                        SaveButton
-                    }
-                }
-            };
-            this.Content = scrollView;
-    }
-    }
-};
+			SaveButton.Clicked += (sender, e) => {
+
+				string tTaskName;
+				string tTaskNotes;
+				DateTime tReminderEndDate;
+				bool tDone;
+				string tRingToneName;
+				string tFrequency;
+				string tFrequencyUnit;
+
+				if (nameEntry.Text != null && noteEntry.Text != null){ 
+				tTaskName = nameEntry.Text.ToString ();
+				tTaskNotes = noteEntry.Text.ToString ();
+				tReminderEndDate = reminderEndDatePicker.Date;
+				tDone = Convert.ToBoolean (donePicker.Items [donePicker.SelectedIndex]);
+				tRingToneName = ringTonePicker.Items [ringTonePicker.SelectedIndex];
+				tFrequency = frequencyPicker.Items [frequencyPicker.SelectedIndex];
+				tFrequencyUnit = frequencyUnitPicker.Items [donePicker.SelectedIndex];
+
+				string Results;
+				Results = tReminderEndDate + " ";
+				DisplayAlert ("Hello, Testing", Results, "Ok");
+
+				//Push the information presented into Scheduler Class. 
+				//Scheduler SchAdd = new Scheduler();
+				//SchAdd.AddTaskWithInfo(tTaskName,tTaskNotes,tReminderEndDate,tRingToneName,tFrequency,tFrequencyUnit);    
+				
+				}
+				else{
+				  DisplayAlert("But wait!","Please write a little about your Task name and task notes","Ok");
+				};
+			};
+			ScrollView scrollView = new ScrollView {
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					Padding = new Thickness (20),
+					Children = {
+						nameLabel,
+						nameEntry,
+						noteLabel,
+						noteEntry,
+						doneLabel,
+						donePicker,
+						ringToneLabel,
+						ringTonePicker,
+						frequencyLabel,
+						frequencyPicker,
+						frequencyUnitLabel,
+						frequencyUnitPicker,
+						reminderEndDateLabel,
+						reminderEndDatePicker,
+						SaveButton
+					}
+				}
+			};
+			this.Content = scrollView;
+		}
+	};
+}
