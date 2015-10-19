005c96a6450b3704fcb6ff9f6a3fb2bf917ecc11
Andy Y <andyyao3@gmail.com>
2015-10-19 13:31:22 -0700

    Worked on utility cs, created ringtone folder

---
+++ ScheduleApp/ScheduleApp/ScheduleApp/Utility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ScheduleApp
+{
+    class Utility
+    {
+        /*Oct 19th 2015 This class will contain at least a 
+        "return list of ringtone" method. The method is used by pageConfig, and 
+        pageAddTask for their ringtone picker.         
+        */
+
+        public List<String> GetRingTones(){
+            //The GetRingTones method will look through the Ringtone folder
+            //loop through all the files that ends in .midi , retrieve their names. 
+            List<String> Results = new List<String>();
+
+            //Results = AssetManager.Open(String);
+            Results = System.IO.Directory.EnumerateDirectories("/");
+           
+            return Results;
+        }
+    }
+}
